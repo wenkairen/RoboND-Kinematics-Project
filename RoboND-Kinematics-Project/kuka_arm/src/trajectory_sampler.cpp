95728313ee5c07efa5a0b576f9042839bca3e32f
WENKAI REN <22199563+wenkairen@users.noreply.github.com>
2018-07-12 16:33:06 +0800

    Update trajectory_sampler.cpp

--- RoboND-Kinematics-Project/kuka_arm/src/trajectory_sampler.cpp
+++ RoboND-Kinematics-Project/kuka_arm/src/trajectory_sampler.cpp
@@ -529,8 +529,8 @@ bool TrajectorySampler::OperateGripper(const bool &close_gripper)
   // Set finger joint values
   if (close_gripper)
   {
-    gripper_joint_positions[0] = 0.02;  // radians
-    gripper_joint_positions[1] = 0.02;  // radians
+    gripper_joint_positions[0] = 0.03;  // radians
+    gripper_joint_positions[1] = 0.03;  // radians
   }
   else
   {
